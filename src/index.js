de60dbb28557dce0113b7d71a594579ef8272218
Stone <sttexa@gmail.com>
2021-12-11 22:01:06 +1100

    Reformat data and check the output of API.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const rssParser=require('./utilities/rss-parser')
+const auDateFormat=require('./utilities/format-iso-date-to-AEST')
 
 const url="https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss";
 
+const reformat=(items)=> {
+  let result=new Array();
+  for (let i=0; i<items.length; i++){
+    result.push({
+      title:items[i].title,
+      audioUrl:items[i].enclosure.url,
+      publishedDate: auDateFormat.convertISODateToAEST(items[i].isoDate)
+    })
+    if(result.length==10) break;
+  }
+  return result;
+}
+
 app.get('/', (req, res) => {
   //res.send('Hello World!')
-  rssParser(url).then(response => res.send(response));
+  rssParser(url).then(response => res.send(reformat(response.items)));
 })
 
 app.listen(port, () => {
