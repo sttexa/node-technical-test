5adaa1076204689bd5bf5867d64dca677c228248
Stone <sttexa@gmail.com>
2021-12-11 21:51:56 +1100

    Install rss-parser package and check out the return of rssParser.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const rssParser=require('./utilities/rss-parser')
+
+const url="https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss";
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  console.log(rssParser(url))
 })
 
 app.listen(port, () => {
